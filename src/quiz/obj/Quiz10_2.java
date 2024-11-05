b6306133b3406ec3f48bc1ddfbf61c6bc156ef04
Admin <Admin@DESKTOP-52351T8>
2024-11-05 17:43:29 +0900

    241105 수업 끝 -6일에는 추상클래스부터

--- src/quiz/obj/Quiz10_2.java
+++ src/quiz/obj/Quiz10_2.java
@@ -7,14 +7,15 @@ public class Quiz10_2 {
 		Warrior w1 = new Warrior("강한친구");
 		Wizard w2 = new Wizard("구르미");
 		Warrior me = new Warrior("나의 캐릭터");
-		Player[] players = new Player[2];
-		players[0] = w1;
-		players[1] = me;
+		Player[] target = new Player[2];
+		target[0] = w1;
+		target[1] = me;
 		
 		
 		//w2의 블리자드를 5번 실행
 		for (int i = 0; i < 5 ; i++) {
-			w2.blizzard(players);
+//			w2.blizzard(target);
+			w2.blizzard(new Player[] {w1,me});
 		}
 	}
 
--- src/quiz/obj/Wizard.java
+++ src/quiz/obj/Wizard.java
@@ -36,11 +36,22 @@ public class Wizard extends Player {
 		 *   3.players배열의 hp를 각각 랜덤난수 만큼 감소
 		 *   4.players배열의 이름과 함께 랜덤피해를 각각 출력.
 		 */
-		for (int i=0; i < players.length; i++) {
-				int dmg = (int)(Math.random() * 6 + 10);
-				players[i].hp -= dmg;
-				System.out.println(players[i].name + "님이 " + dmg + "피해를 입었습니다.");
+		
+		//1. for문 사용(강화된 for문)
+		for(Player play:players) {
+			// 2. 난수 생성
+			int damage = (int)(Math.random()*6) + 10;	// 10 ~ 15
+			// 3. hp 감소
+			play.hp -= damage;
+			// 4. 감소 내용 출력
+			System.out.println(play.name + "님이 " + damage + "피해를 입었습니다.");
 		}
+
+//		for (int i=0; i < players.length; i++) {
+//				int dmg = (int)(Math.random() * 6 + 10);
+//				players[i].hp -= dmg;
+//				System.out.println(players[i].name + "님이 " + dmg + "피해를 입었습니다.");
+//		}
 	}
 		
 		
