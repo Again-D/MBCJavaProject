6bdc3bf5a360313739c79a2fa3b3184c6231c08c
Admin <Admin@DESKTOP-52351T8>
2024-11-19 13:22:19 +0900

    1119 오전 ch18

--- src/study/First2.java
+++ src/study/First2.java
@@ -1,18 +1,16 @@
 package study;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class First2 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  throws Exception{
 		// TODO Auto-generated method stub
-		try {
-			ageCal(1999,2030,"c");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
 		
-
+			ageCal(1999,2030,"k");
+			System.out.println("해당 년도에 "+ageCal(1999,2030,"k")+ "세 입니다");
+		
 	}
 	
 	static int ageCal(int birthYear,int baseYear,String ageType) throws Exception {
